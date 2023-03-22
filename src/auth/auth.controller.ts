e7b5155e6e4d647530eeb98f049d0830c863ccd6
samiulislam09 <samiulislamsawon09@gmail.com>
2023-03-22 18:34:09 +0600

    added auth

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,35 @@
+import {
+  Body,
+  Controller,
+  Post,
+  UseGuards,
+  Request,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './local-auth.guard';
+import { JwtAuthGuard } from './jwt.strategy';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('register')
+  async register(
+    @Body('username') username: string,
+    @Body('password') password: string,
+  ) {
+    return this.authService.register(username, password);
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('profile')
+  async profile(@Request() req) {
+    return req.user;
+  }
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from '../user/user.service';
+import { User } from '../user/user.entity';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  async register(username: string, password: string): Promise<User> {
+    return this.userService.create(username, password);
+  }
+
+  async validateUser(
+    username: string,
+    password: string,
+  ): Promise<Omit<User, 'password'> | null> {
+    const user = await this.userService.findOne(username);
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
+  async login(user: User) {
+    const payload = { username: user.username };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
