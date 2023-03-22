6ee3756d1404d1a23a122fd624a160b7155f290a
samiulislam09 <samiulislamsawon09@gmail.com>
2023-03-22 18:34:09 +0600

    added auth

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,24 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { UserService } from '../user/user.service';
+import { User } from '../user/user.entity';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private userService: UserService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(payload: any): Promise<User> {
+    const { username } = payload;
+    const user = await this.userService.findOne(username);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+}
